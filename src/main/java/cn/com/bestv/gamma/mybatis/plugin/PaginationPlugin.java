f1a039effe73bd9d8da8155237b0a03e8d6748d7
yan.kefei <yan.kefei@opg.cn>
2017-12-07 14:05:56 +0800

    修复生成xml文件无法覆盖的问题

--- src/main/java/cn/com/bestv/gamma/mybatis/plugin/PaginationPlugin.java
+++ src/main/java/cn/com/bestv/gamma/mybatis/plugin/PaginationPlugin.java
@@ -3,6 +3,7 @@ package cn.com.bestv.gamma.mybatis.plugin;
 import java.util.List;
 
 import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.GeneratedXmlFile;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
@@ -17,65 +18,73 @@ import org.mybatis.generator.api.dom.xml.XmlElement;
 
 public class PaginationPlugin extends PluginAdapter {
 
-	@Override
-	public boolean modelExampleClassGenerated(TopLevelClass topLevelClass,
-			IntrospectedTable introspectedTable) {
-		// add field, getter, setter for limit clause
-		addLimit(topLevelClass, introspectedTable, "pageStart");
-		addLimit(topLevelClass, introspectedTable, "pageSize");
-		return super.modelExampleClassGenerated(topLevelClass,
-				introspectedTable);
-	}
+    @Override
+    public boolean modelExampleClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        // add field, getter, setter for limit clause
+        addLimit(topLevelClass, introspectedTable, "pageStart");
+        addLimit(topLevelClass, introspectedTable, "pageSize");
+        return super.modelExampleClassGenerated(topLevelClass, introspectedTable);
+    }
 
-	@Override
-	public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(
-			XmlElement element, IntrospectedTable introspectedTable) {
-		// XmlElement isParameterPresenteElemen = (XmlElement) element
-		// .getElements().get(element.getElements().size() - 1);
-		XmlElement isNotNullElement = new XmlElement("if"); //$NON-NLS-1$  
-		isNotNullElement.addAttribute(new Attribute(
-				"test", "pageStart != null and pageSize>=0")); //$NON-NLS-1$ //$NON-NLS-2$  
-		//      isNotNullElement.addAttribute(new Attribute("compareValue", "0")); //$NON-NLS-1$ //$NON-NLS-2$  
-		isNotNullElement.addElement(new TextElement(
-				"limit #{pageStart} , #{pageSize}"));
-		// isParameterPresenteElemen.addElement(isNotNullElement);
-		element.addElement(isNotNullElement);
-		return super.sqlMapUpdateByExampleWithoutBLOBsElementGenerated(element,
-				introspectedTable);
-	}
+    @Override
+    public boolean sqlMapSelectByExampleWithoutBLOBsElementGenerated(XmlElement element,
+            IntrospectedTable introspectedTable) {
+        // XmlElement isParameterPresenteElemen = (XmlElement) element
+        // .getElements().get(element.getElements().size() - 1);
+        XmlElement isNotNullElement = new XmlElement("if"); //$NON-NLS-1$  
+        isNotNullElement.addAttribute(new Attribute("test", "pageStart != null and pageSize>=0")); //$NON-NLS-1$ //$NON-NLS-2$  
+        //      isNotNullElement.addAttribute(new Attribute("compareValue", "0")); //$NON-NLS-1$ //$NON-NLS-2$  
+        isNotNullElement.addElement(new TextElement("limit #{pageStart} , #{pageSize}"));
+        // isParameterPresenteElemen.addElement(isNotNullElement);
+        element.addElement(isNotNullElement);
+        return super.sqlMapUpdateByExampleWithoutBLOBsElementGenerated(element, introspectedTable);
+    }
 
-	private void addLimit(TopLevelClass topLevelClass,
-			IntrospectedTable introspectedTable, String name) {
-		CommentGenerator commentGenerator = context.getCommentGenerator();
-		Field field = new Field();
-		field.setVisibility(JavaVisibility.PROTECTED);
-		// field.setType(FullyQualifiedJavaType.getIntInstance());
-		field.setType(PrimitiveTypeWrapper.getIntegerInstance());
-		field.setName(name);
-		// field.setInitializationString("-1");
-		commentGenerator.addFieldComment(field, introspectedTable);
-		topLevelClass.addField(field);
-		char c = name.charAt(0);
-		String camel = Character.toUpperCase(c) + name.substring(1);
-		Method method = new Method();
-		method.setVisibility(JavaVisibility.PUBLIC);
-		method.setName("set" + camel);
-		method.addParameter(new Parameter(PrimitiveTypeWrapper
-				.getIntegerInstance(), name));
-		method.addBodyLine("this." + name + "=" + name + ";");
-		commentGenerator.addGeneralMethodComment(method, introspectedTable);
-		topLevelClass.addMethod(method);
-		method = new Method();
-		method.setVisibility(JavaVisibility.PUBLIC);
-		method.setReturnType(PrimitiveTypeWrapper.getIntegerInstance());
-		method.setName("get" + camel);
-		method.addBodyLine("return " + name + ";");
-		commentGenerator.addGeneralMethodComment(method, introspectedTable);
-		topLevelClass.addMethod(method);
-	}
+    private void addLimit(TopLevelClass topLevelClass, IntrospectedTable introspectedTable, String name) {
+        CommentGenerator commentGenerator = context.getCommentGenerator();
+        Field field = new Field();
+        field.setVisibility(JavaVisibility.PROTECTED);
+        // field.setType(FullyQualifiedJavaType.getIntInstance());
+        field.setType(PrimitiveTypeWrapper.getIntegerInstance());
+        field.setName(name);
+        // field.setInitializationString("-1");
+        commentGenerator.addFieldComment(field, introspectedTable);
+        topLevelClass.addField(field);
+        char c = name.charAt(0);
+        String camel = Character.toUpperCase(c) + name.substring(1);
+        Method method = new Method();
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setName("set" + camel);
+        method.addParameter(new Parameter(PrimitiveTypeWrapper.getIntegerInstance(), name));
+        method.addBodyLine("this." + name + "=" + name + ";");
+        commentGenerator.addGeneralMethodComment(method, introspectedTable);
+        topLevelClass.addMethod(method);
+        method = new Method();
+        method.setVisibility(JavaVisibility.PUBLIC);
+        method.setReturnType(PrimitiveTypeWrapper.getIntegerInstance());
+        method.setName("get" + camel);
+        method.addBodyLine("return " + name + ";");
+        commentGenerator.addGeneralMethodComment(method, introspectedTable);
+        topLevelClass.addMethod(method);
+    }
 
-	public boolean validate(List<String> arg0) {
-		return true;
-	}
+    /**
+     * 修复生成xml文件不能覆盖的问题
+     */
+    @Override
+    public boolean sqlMapGenerated(GeneratedXmlFile sqlMap, IntrospectedTable introspectedTable) {
+        try {
+            java.lang.reflect.Field field = sqlMap.getClass().getDeclaredField("isMergeable");
+            field.setAccessible(true);
+            field.setBoolean(sqlMap, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean validate(List<String> arg0) {
+        return true;
+    }
 
 }
